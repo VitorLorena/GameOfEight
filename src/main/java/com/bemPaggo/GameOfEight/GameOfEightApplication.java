f8c10fb8761fa472dcc923de24da91cdb7f05f2b
VitorDani <vitord2002@hotmail.com>
2022-03-11 16:00:36 -0300

    Ultimos ajustes do jogo

---
+++ src/main/java/com/bemPaggo/GameOfEight/GameOfEightApplication.java
@@ -0,0 +1,13 @@
+package com.bemPaggo.GameOfEight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfEightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfEightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemPaggo/GameOfEight/GameOfEightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemPaggo.GameOfEight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfEightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
